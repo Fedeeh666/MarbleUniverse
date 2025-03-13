469c37891662b33b1499124c411587ebfa95695f
Fedeeh666 <pre47cher@gmail.com>
2025-03-13 04:25:37 -0300

    Add files via upload

--- scripts/c3main.js
+++ scripts/c3main.js
@@ -1,4 +1,4 @@
-import "./../box2d.wasm.js";
 import "./c3runtime.js";
 import "./plugins/LostTelegramMiniAppsSDK/c3runtime/main.js";
 import "./objRefTable.js";
+import "./project/scriptsInEvents.js";
